df71538e7d6d4e3a71958d9a364223493e9c4a01
ErmushinRoman <Ermushin Roman>
2023-08-22 20:55:09 +0300

    ftf

--- appRender.js
+++ appRender.js
@@ -3,15 +3,16 @@ import { getFunction } from "./modules/getFunction.js";
 import { commentsRender } from "./modules/render.js";
 import { commentators, loader, newIsLoginForLike } from "./modules/variables.js";
 
+
 "use strict";
 
-//Данная функция принимаетв себя переменную, которая = true or false и отрисовывает разметку внутри единственной разметки в HTML, для того, чтобы потом отрисовать туда следующую разметку
-// блок отвечает только за страницу с комментариями
+//?Данная функция принимаетв себя переменную, которая = true or false и отрисовывает разметку внутри единственной разметки в HTML, для того, чтобы потом отрисовать туда следующую разметку
+// ?блок отвечает только за страницу с комментариями
 export function appRender(isLogin, loader) {
   newIsLoginForLike(isLogin)
-  const app = document.querySelector('.app');//Нашли единственный элемент в HTML
+  const app = document.querySelector('.app');//?Нашли единственный элемент в HTML
   if (isLogin) {
-    //Отрисовываем разметку >
+    //?Отрисовываем разметку >
     app.innerHTML = `<div class="container">
         <ul id="comment-block" class="comments">
           <!-- render -->
@@ -20,7 +21,7 @@ export function appRender(isLogin, loader) {
           <!-- render -->
         </div>
       </div>`;
-    getFunction(commentators, loader);//после того, как отрисовали разметку запускаем getFunction(смотри процесс в getFunction.js)
+    getFunction(commentators, loader);//?после того, как отрисовали разметку запускаем getFunction(смотри процесс в getFunction.js)
     commentsRender(commentators);
   } else {
     app.innerHTML = `<div class="container">
@@ -32,14 +33,14 @@ export function appRender(isLogin, loader) {
   </div>
   <div class="start-form">Для того, чтобы войти нажми ><span id="sign-in" class="menu">Войти</span></div>
 </div>`;
-    //для того, чтобы перейти на форму авторизации надо найти sign-in и на него повесить обработчик событий
-    getFunction(commentators, loader);//после того, как отрисовали разметку запускаем getFunction(смотри процесс в getFunction.js)
-    commentsRender(commentators);//render.js
-    // autorisation()
-    const addForm = document.getElementById('addFormElement');// данным действием мы убираем форму и вместо  нее ставим ссылку
+    //?для того, чтобы перейти на форму авторизации надо найти sign-in и на него повесить обработчик событий
+    getFunction(commentators, loader);//?после того, как отрисовали разметку запускаем getFunction(смотри процесс в getFunction.js)
+    commentsRender(commentators);//?render.js
+    //? autorisation()
+    const addForm = document.getElementById('addFormElement');//? данным действием мы убираем форму и вместо  нее ставим ссылку
     console.log(addForm);
-    addForm.style.display = `none`;//мы обращаемся к переменной addForm, и задаем ей дисплей нон, благодаря 
-    const signIn = document.getElementById('sign-in');//определяем, так как разметка отрисовалась
+    addForm.style.display = `none`;//?мы обращаемся к переменной addForm, и задаем ей дисплей нон, благодаря 
+    const signIn = document.getElementById('sign-in');//?определяем, так как разметка отрисовалась
     signIn.addEventListener('click', () => {
       autorisation()
     })
--- modules/getFunction.js
+++ modules/getFunction.js
@@ -5,7 +5,8 @@ import { getComments } from "./api.js";
 export function getFunction(commentators, loader) {
   loader = true;// Тут, пока страница не загрузилась, индикатор по умолчанию будет = false, и как только функция дошла до выполнения < этого кода индикатор = true появляется форма >
   formRender(loader);//Запустили formRender(смотри процесс в render.js )
-  getComments().then((responseData) => {//getComments(api.js) вызвали ее и она нам вернула промис с объектом в котором сохраненные данные с комментариями с сервера в формате json
+  getComments().then((responseData) => {
+    console.log(responseData.comments);//getComments(api.js) вызвали ее и она нам вернула промис с объектом в котором сохраненные данные с комментариями с сервера в формате json
     commentators = responseData.comments;// в аргумент, который явл. переменной массива, помещаем данные из объекта, которые внутри массива, внутри которого объекты с ключами, некоторы из которых и являются текстом комментария
     commentsRender(commentators);//запускаем commentsRender(render.js) в аргумент которой и идет наш массив с данными с сервера, это и отрисовывает наши комментарии
     loader = false;
